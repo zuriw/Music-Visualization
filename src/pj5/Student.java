392a2584947c9a733d6723518e5a97727f8df35e
uzia35 <usmana@vt.edu>
2016-11-30 17:56:27 -0500

    added java docs

--- src/pj5/Student.java
+++ src/pj5/Student.java
@@ -1,21 +1,32 @@
-/**
- * 
- */
 package pj5;
 
 /**
- * @author zuriw
+ * This class creates a Student Object that consists of a major, hobby, and
+ * region with their Songs heard and Songs liked *
+ * 
+ * @author zuriw,usmana
  * @version 2016.11.23
  */
 public class Student
 {
 
-    private String major;
-    private String hobby;
-    private String region;
+    private String      major;
+    private String      hobby;
+    private String      region;
     private LList<Song> songsHeard;
     private LList<Song> songsLike;
 
+
+    /**
+     * Constructor for the Student Object
+     * 
+     * @param majorIn
+     *            major of the Student
+     * @param hobbyIn
+     *            hobby of the Student
+     * @param regionIn
+     *            region of the Student
+     */
     public Student(String majorIn, String hobbyIn, String regionIn)
     {
         major = majorIn;
@@ -26,45 +37,90 @@ public class Student
     }
 
 
+    /**
+     * gets the Major of the Student
+     * 
+     * @return Major of the Student
+     */
     public String getMajor()
     {
         return major;
     }
 
 
+    /**
+     * gets the Hobby of the Student
+     * 
+     * @return Hobby of the Student
+     */
     public String getHobby()
     {
         return hobby;
     }
 
 
+    /**
+     * gets the Region of the Student
+     * 
+     * @return Region of the Student
+     */
     public String getRegion()
     {
         return region;
     }
-    
-    public LList getSongsHeard()
+
+
+    /**
+     * gets the LinkedList of Songs Heard
+     * 
+     * @return the LList<Song> of Songs Heard
+     */
+    public LList<Song> getSongsHeard()
     {
         return songsHeard;
     }
-    
-    public LList getSongsLike()
+
+
+    /**
+     * gets the LinkedList of Songs Liked
+     * 
+     * @return the LList<Song> of Songs Liked
+     */
+    public LList<Song> getSongsLike()
     {
         return songsLike;
     }
-    
+
+
+    /**
+     * adds to the List of Songs Heard
+     * 
+     * @param songIn
+     *            Song to be inputed
+     */
     public void addHeard(Song songIn)
     {
         songsHeard.add(songIn);
     }
-    
+
+
+    /**
+     * adds to the List of Songs Liked
+     * 
+     * @param songIn
+     *            Song to be inputed
+     */
     public void addLike(Song songIn)
     {
         songsLike.add(songIn);
     }
-    
+
+
     /**
+     * Creates and returns a String representation of a Student *
      * 
+     * @return the Song in a String format Studejt Major: Student Region:
+     *         Student Hobby:
      */
     public String toString()
     {
