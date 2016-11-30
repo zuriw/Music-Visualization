521f6c8a37d817750f42a48017254b3170f6daa3
uzia35 <usmana@vt.edu>
2016-11-30 17:34:08 -0500

    added java doc

--- src/pj5/CompareGenre.java
+++ src/pj5/CompareGenre.java
@@ -1,17 +1,23 @@
-/**
- * 
- */
+
 package pj5;
 
 import java.util.Comparator;
 
 /**
- * @author zuriw
- *
+ * Comparator class that compares two Song Objects by their Genre
+ * 
+ * @author usmana
+ * @version 11/30/2016
  */
 public class CompareGenre implements Comparator<Song>
 {
 
+    /**
+     * Compares two Song Objects by the Genre and returns less than /equal to  / greater than
+     * @param song1 Song Object 1 
+     * @param song2 the other Song Object being compared to
+     * @return int negative (less) 0 (equal to) positive (greater than)  Ignores String case
+     */
     @Override
     public int compare(Song song1, Song song2)
     {
