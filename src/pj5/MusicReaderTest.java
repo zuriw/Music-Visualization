2e65c3533df848df16a55305ce6d88eca7ad9d2b
uzia35 <usmana@vt.edu>
2016-11-30 17:49:46 -0500

    added test case and java docs

--- src/pj5/MusicReaderTest.java
+++ src/pj5/MusicReaderTest.java
@@ -1,31 +1,38 @@
-/**
- * 
- */
 package pj5;
 import student.TestCase;
 
 import java.io.FileNotFoundException;
 
 /**
- * @author zuriw
+ * This is a test class for MusicReader
+ * @author zuriw,usmana
+ * @version 11/30/2016
  *
  */
 public class MusicReaderTest extends TestCase
 {
 
     private MusicReader reader;
+    /**
+     * creates an MusicReader object for testing
+     */
     public void setUp() throws FileNotFoundException
     {
         reader = new MusicReader("SongListTest1.csv");
     }
     
+    /**
+     * tests to see if the MusicReader can retrieve the songs from the File
+     */
     public void testGetSongs()
     {
         
-        LList songs = reader.getSongs();
-        for (int i = 1; i <= songs.getLength(); i++)
+        LList<Song> songs = reader.getSongs();
+       /* for (int i = 1; i <= songs.getLength(); i++)
         {
             System.out.println(songs.getEntry(i).toString());
-        }
+        }*/
+        assertEquals(5,songs.getLength());
     }
 }
+
