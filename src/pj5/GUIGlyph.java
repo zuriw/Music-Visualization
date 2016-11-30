185259408161227007e9ba9f5069914046f89ab4
jchu247 <jchu247@vt.edu>
2016-11-29 20:03:58 -0500

    Updated GUIGlyph

--- src/pj5/GUIGlyph.java
+++ src/pj5/GUIGlyph.java
@@ -88,9 +88,14 @@ public class GUIGlyph
     private TextShape TitleOfSong;
     private TextShape heard;
     private TextShape likes;
+    private boolean hasBlackBars;
+    private LList<Song> musicReader;
+    private LList<Student> surveyReader;
+    private int songTracker;
     
-    public GUIGlyph()
+    public GUIGlyph(LList<Song> music, LList<Student> survey)
     {
+        hasBlackBars = false;
         mainWindow = new Window("Project 5");
         buttonExit = new Button("Quit");
         buttonHobbies = new Button("Represent Hobby");
@@ -102,7 +107,35 @@ public class GUIGlyph
         buttonSortSongTitle = new Button("Sort by Song Title");
         buttonSortYear = new Button("Sort by Release Year");
         buttonSortGenre = new Button("Sort by Genre");
-        blackBar1 = new Shape(100, 50, 10, 40, Color.BLACK);
+        
+        buttonExit.onClick(this, "clickedExit");
+        buttonHobbies.onClick(this, "clickedHobbies");
+        buttonMajor.onClick(this, "clickedMajor");
+        buttonRegion.onClick(this, "clickedRegion");
+        buttonPrevious.onClick(this, "clickedPrevious");
+        buttonNext.onClick(this, "clickedNext");
+        buttonSortArtist.onClick(this, "clickedSortByArtist");
+        buttonSortSongTitle.onClick(this, "clickedSortBySongTitle");
+        buttonSortYear.onClick(this, "clickedSortByYear");
+        buttonSortGenre.onClick(this, "clickedSortByGenre");
+        
+        mainWindow.addButton(buttonPrevious, WindowSide.NORTH);
+        mainWindow.addButton(buttonSortArtist, WindowSide.NORTH);
+        mainWindow.addButton(buttonSortSongTitle, WindowSide.NORTH);
+        mainWindow.addButton(buttonSortYear, WindowSide.NORTH);
+        mainWindow.addButton(buttonSortGenre, WindowSide.NORTH);
+        mainWindow.addButton(buttonNext, WindowSide.NORTH);
+        mainWindow.addButton(buttonHobbies, WindowSide.SOUTH);
+        mainWindow.addButton(buttonMajor, WindowSide.SOUTH);
+        mainWindow.addButton(buttonRegion, WindowSide.SOUTH);
+        mainWindow.addButton(buttonExit, WindowSide.SOUTH);
+        
+        musicReader = music;
+     
+        surveyReader = survey;
+        songTracker = 0;
+        
+        /*blackBar1 = new Shape(100, 50, 10, 40, Color.BLACK);
         blackBar2 = new Shape(300, 50, 10, 40, Color.BLACK);
         blackBar3 = new Shape(500, 50, 10, 40, Color.BLACK);
         blackBar4 = new Shape(100, 150, 10, 40, Color.BLACK);
@@ -157,28 +190,14 @@ public class GUIGlyph
         shapeLegend7 = new Shape(75, 280, 60, 10, Color.GREEN);
         shapeLegend8 = new Shape(275, 280, 60, 10, Color.GREEN);
         shapeLegend9 = new Shape(475, 280, 60, 10, Color.GREEN);
-        legend1 = new Shape(600, 150, 5, 185, Color.BLACK);
-        legend2 = new Shape(720, 150, 5, 185, Color.BLACK);
-        legend3 = new Shape(600, 150, 120, 5, Color.BLACK);
-        legend = new Shape(600, 330, 120, 5, Color.BLACK);
+        
         isLegend = new TextShape(610, 160, "Legend", Color.BLACK);
         options1 = new TextShape(610, 180, "Option 1", Color.MAGENTA);
         options2 = new TextShape(610, 200, "Option 2", Color.BLUE);
         options3 = new TextShape(610, 220, "Option 3", Color.YELLOW);
         options4 = new TextShape(610, 240, "Option 4", Color.GREEN);
-        TitleOfSong = new TextShape(655, 260, "Song", Color.BLACK);
-        heard = new TextShape(610, 290, "Heard", Color.BLACK);
-        likes = new TextShape(680, 290, "Likes", Color.BLACK);
-        mainWindow.addButton(buttonPrevious, WindowSide.NORTH);
-        mainWindow.addButton(buttonSortArtist, WindowSide.NORTH);
-        mainWindow.addButton(buttonSortSongTitle, WindowSide.NORTH);
-        mainWindow.addButton(buttonSortYear, WindowSide.NORTH);
-        mainWindow.addButton(buttonSortGenre, WindowSide.NORTH);
-        mainWindow.addButton(buttonNext, WindowSide.NORTH);
-        mainWindow.addButton(buttonHobbies, WindowSide.SOUTH);
-        mainWindow.addButton(buttonMajor, WindowSide.SOUTH);
-        mainWindow.addButton(buttonRegion, WindowSide.SOUTH);
-        mainWindow.addButton(buttonExit, WindowSide.SOUTH);
+        
+        
         mainWindow.addShape(blackBar1);
         mainWindow.addShape(blackBar2);
         mainWindow.addShape(blackBar3);
@@ -233,48 +252,194 @@ public class GUIGlyph
         mainWindow.addShape(shapeLegend7);
         mainWindow.addShape(shapeLegend8);
         mainWindow.addShape(shapeLegend9);
-        mainWindow.addShape(legend);
-        mainWindow.addShape(legend1);
-        mainWindow.addShape(legend2);
-        mainWindow.addShape(legend3);
+        
+        
+        */
+    }
+    
+    public void clickedExit(Button button)
+    {
+        System.exit(0);
+    }
+    
+    public void clickedNext(Button button)
+    {
+        mainWindow.removeShape(songTitle1);
+        mainWindow.removeShape(songTitle2);
+        mainWindow.removeShape(songTitle3);
+        mainWindow.removeShape(songTitle4);
+        mainWindow.removeShape(songTitle5);
+        mainWindow.removeShape(songTitle6);
+        mainWindow.removeShape(songTitle7);
+        mainWindow.removeShape(songTitle8);
+        mainWindow.removeShape(songTitle9);
+        songTracker++;
+        songTitle1 = new TextShape(80, 30, musicReader.getEntry(1 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle1);
+        songTitle2 = new TextShape(280, 30, musicReader.getEntry(2 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle2);
+        songTitle3 = new TextShape(480, 30, musicReader.getEntry(3 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle3);
+        songTitle4 = new TextShape(80, 130, musicReader.getEntry(4 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle4);
+        songTitle5 = new TextShape(280, 130, musicReader.getEntry(5 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle5);
+        songTitle6 = new TextShape(480, 130, musicReader.getEntry(6 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle6);
+        songTitle7 = new TextShape(80, 230, musicReader.getEntry(7 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle7);
+        songTitle8 = new TextShape(280, 230, musicReader.getEntry(8 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle8);
+        songTitle9 = new TextShape(480, 230, musicReader.getEntry(9 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle9);
+        
+    }
+    
+    public void clickedHobbies(Button button)
+    {
+        if (!hasBlackBars)
+        { 
+            drawBlackBars();
+            hasBlackBars = true;
+            addInitialSongs();
+        }
+        else
+        {
+            mainWindow.removeShape(isLegend);
+            mainWindow.removeShape(options1);
+            mainWindow.removeShape(options2);
+            mainWindow.removeShape(options3);
+            mainWindow.removeShape(options4);
+            mainWindow.removeShape(songTitle1);
+            mainWindow.removeShape(songTitle2);
+            mainWindow.removeShape(songTitle3);
+            mainWindow.removeShape(songTitle4);
+            mainWindow.removeShape(songTitle5);
+            mainWindow.removeShape(songTitle6);
+            mainWindow.removeShape(songTitle7);
+            mainWindow.removeShape(songTitle8);
+            mainWindow.removeShape(songTitle9);
+            addInitialSongs();
+        }
+        isLegend = new TextShape(610, 160, "Hobby Legend", Color.BLACK);
+        options1 = new TextShape(610, 180, "Read", Color.MAGENTA);
+        options2 = new TextShape(610, 200, "Art", Color.BLUE);
+        options3 = new TextShape(610, 220, "Sports", Color.YELLOW);
+        options4 = new TextShape(610, 240, "Music", Color.GREEN);
         mainWindow.addShape(isLegend);
         mainWindow.addShape(options1);
         mainWindow.addShape(options2);
         mainWindow.addShape(options3);
         mainWindow.addShape(options4);
-        mainWindow.addShape(TitleOfSong);
-        mainWindow.addShape(blackBar10);
-        mainWindow.addShape(likes);
-        mainWindow.addShape(heard);
-    }
-    
-    public void clickedExit(Button button)
-    {
-        
-    }
-    
-    public void clickedNext(Button button)
-    {
-        
-    }
-    public void clickedHobbies(Button button)
-    {
-        
+
     }
     
     public void clickedMajor(Button button)
     {
-        
+        if (!hasBlackBars)
+        { 
+            drawBlackBars();
+            hasBlackBars = true;
+            addInitialSongs();
+        }
+        else
+        {
+            mainWindow.removeShape(isLegend);
+            mainWindow.removeShape(options1);
+            mainWindow.removeShape(options2);
+            mainWindow.removeShape(options3);
+            mainWindow.removeShape(options4);
+            mainWindow.removeShape(songTitle1);
+            mainWindow.removeShape(songTitle2);
+            mainWindow.removeShape(songTitle3);
+            mainWindow.removeShape(songTitle4);
+            mainWindow.removeShape(songTitle5);
+            mainWindow.removeShape(songTitle6);
+            mainWindow.removeShape(songTitle7);
+            mainWindow.removeShape(songTitle8);
+            mainWindow.removeShape(songTitle9);
+            addInitialSongs();
+        }
+        isLegend = new TextShape(610, 160, "Major Legend", Color.BLACK);
+        options1 = new TextShape(610, 180, "Comp Sci", Color.MAGENTA);
+        options2 = new TextShape(610, 200, "Other Eng", Color.BLUE);
+        options3 = new TextShape(610, 220, "Math/CDMA", Color.YELLOW);
+        options4 = new TextShape(610, 240, "Other", Color.GREEN);
+        mainWindow.addShape(isLegend);
+        mainWindow.addShape(options1);
+        mainWindow.addShape(options2);
+        mainWindow.addShape(options3);
+        mainWindow.addShape(options4);
     }
     
     public void clickedRegion(Button button)
     {
-        
+        if (!hasBlackBars)
+        { 
+            drawBlackBars();
+            hasBlackBars = true;
+            addInitialSongs();
+        }
+        else
+        {
+            mainWindow.removeShape(isLegend);
+            mainWindow.removeShape(options1);
+            mainWindow.removeShape(options2);
+            mainWindow.removeShape(options3);
+            mainWindow.removeShape(options4);
+            mainWindow.removeShape(songTitle1);
+            mainWindow.removeShape(songTitle2);
+            mainWindow.removeShape(songTitle3);
+            mainWindow.removeShape(songTitle4);
+            mainWindow.removeShape(songTitle5);
+            mainWindow.removeShape(songTitle6);
+            mainWindow.removeShape(songTitle7);
+            mainWindow.removeShape(songTitle8);
+            mainWindow.removeShape(songTitle9);
+            addInitialSongs();
+        }
+        isLegend = new TextShape(610, 160, "Region Legend", Color.BLACK);
+        options1 = new TextShape(610, 180, "NE US", Color.MAGENTA);
+        options2 = new TextShape(610, 200, "SE US", Color.BLUE);
+        options3 = new TextShape(610, 220, "Rest of US", Color.YELLOW);
+        options4 = new TextShape(610, 240, "Outside the US", Color.GREEN);
+        mainWindow.addShape(isLegend);
+        mainWindow.addShape(options1);
+        mainWindow.addShape(options2);
+        mainWindow.addShape(options3);
+        mainWindow.addShape(options4);
     }
     
     public void clickedPrevious(Button button)
     {
-        
+        mainWindow.removeShape(songTitle1);
+        mainWindow.removeShape(songTitle2);
+        mainWindow.removeShape(songTitle3);
+        mainWindow.removeShape(songTitle4);
+        mainWindow.removeShape(songTitle5);
+        mainWindow.removeShape(songTitle6);
+        mainWindow.removeShape(songTitle7);
+        mainWindow.removeShape(songTitle8);
+        mainWindow.removeShape(songTitle9);
+        songTracker--;
+        songTitle1 = new TextShape(80, 30, musicReader.getEntry(1 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle1);
+        songTitle2 = new TextShape(280, 30, musicReader.getEntry(2 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle2);
+        songTitle3 = new TextShape(480, 30, musicReader.getEntry(3 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle3);
+        songTitle4 = new TextShape(80, 130, musicReader.getEntry(4 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle4);
+        songTitle5 = new TextShape(280, 130, musicReader.getEntry(5 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle5);
+        songTitle6 = new TextShape(480, 130, musicReader.getEntry(6 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle6);
+        songTitle7 = new TextShape(80, 230, musicReader.getEntry(7 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle7);
+        songTitle8 = new TextShape(280, 230, musicReader.getEntry(8 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle8);
+        songTitle9 = new TextShape(480, 230, musicReader.getEntry(9 + 9 * songTracker).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle9);
     }
     
     public void clickedSortByArtist(Button button)
@@ -295,6 +460,68 @@ public class GUIGlyph
     public void clickedSortByGenre(Button button)
     {
         
+    }
+    
+    private void drawBlackBars()
+    {
+        blackBar1 = new Shape(100, 50, 10, 40, Color.BLACK);
+        blackBar2 = new Shape(300, 50, 10, 40, Color.BLACK);
+        blackBar3 = new Shape(500, 50, 10, 40, Color.BLACK);
+        blackBar4 = new Shape(100, 150, 10, 40, Color.BLACK);
+        blackBar5 = new Shape(300, 150, 10, 40, Color.BLACK);
+        blackBar6 = new Shape(500, 150, 10, 40, Color.BLACK);
+        blackBar7 = new Shape(100, 250, 10, 40, Color.BLACK);
+        blackBar8 = new Shape(300, 250, 10, 40, Color.BLACK);
+        blackBar9 = new Shape(500, 250, 10, 40, Color.BLACK);
+        blackBar10 = new Shape(665, 280, 10, 40, Color.BLACK);
+        mainWindow.addShape(blackBar1);
+        mainWindow.addShape(blackBar2);
+        mainWindow.addShape(blackBar3);
+        mainWindow.addShape(blackBar4);
+        mainWindow.addShape(blackBar5);
+        mainWindow.addShape(blackBar6);
+        mainWindow.addShape(blackBar7);
+        mainWindow.addShape(blackBar8);
+        mainWindow.addShape(blackBar9);
+        mainWindow.addShape(blackBar10);
+        legend1 = new Shape(600, 150, 5, 185, Color.BLACK);
+        legend2 = new Shape(720, 150, 5, 185, Color.BLACK);
+        legend3 = new Shape(600, 150, 120, 5, Color.BLACK);
+        legend = new Shape(600, 330, 120, 5, Color.BLACK);
+        mainWindow.addShape(legend);
+        mainWindow.addShape(legend1);
+        mainWindow.addShape(legend2);
+        mainWindow.addShape(legend3);
+        TitleOfSong = new TextShape(655, 260, "Song", Color.BLACK);
+        heard = new TextShape(610, 290, "Heard", Color.BLACK);
+        likes = new TextShape(680, 290, "Likes", Color.BLACK);
+        mainWindow.addShape(TitleOfSong);
+        mainWindow.addShape(likes);
+        mainWindow.addShape(heard);
+       
+    }
+    
+    private  void addInitialSongs()
+    {
+        songTracker = 0;
+        songTitle1 = new TextShape(80, 30, musicReader.getEntry(1).getTitle(), Color.BLACK);
+        songTitle2 = new TextShape(280, 30, musicReader.getEntry(2).getTitle(), Color.BLACK);
+        songTitle3 = new TextShape(480, 30, musicReader.getEntry(3).getTitle(), Color.BLACK);
+        songTitle4 = new TextShape(80, 130, musicReader.getEntry(4).getTitle(), Color.BLACK);
+        songTitle5 = new TextShape(280, 130, musicReader.getEntry(5).getTitle(), Color.BLACK);
+        songTitle6 = new TextShape(480, 130, musicReader.getEntry(6).getTitle(), Color.BLACK);
+        songTitle7 = new TextShape(80, 230, musicReader.getEntry(7).getTitle(), Color.BLACK);
+        songTitle8 = new TextShape(280, 230, musicReader.getEntry(8).getTitle(), Color.BLACK);
+        songTitle9 = new TextShape(480, 230, musicReader.getEntry(9).getTitle(), Color.BLACK);
+        mainWindow.addShape(songTitle1);
+        mainWindow.addShape(songTitle2);
+        mainWindow.addShape(songTitle3);
+        mainWindow.addShape(songTitle4);
+        mainWindow.addShape(songTitle5);
+        mainWindow.addShape(songTitle6);
+        mainWindow.addShape(songTitle7);
+        mainWindow.addShape(songTitle8);
+        mainWindow.addShape(songTitle9);
     }
 }
 
