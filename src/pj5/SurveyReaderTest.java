c24e27d289572ac80a9c23e3a6e3c389c7adb5ee
uzia35 <usmana@vt.edu>
2016-11-30 22:29:33 -0500

    added java docs

--- src/pj5/SurveyReaderTest.java
+++ src/pj5/SurveyReaderTest.java
@@ -1,13 +1,13 @@
-/**
- * 
- */
 package pj5;
 
 import java.io.FileNotFoundException;
 import student.TestCase;
 
 /**
- * @author zuriw
+ * This is a test class for SurveyReader
+ * 
+ * @author zuriw, usmana
+ * @version 11/30/2016
  */
 public class SurveyReaderTest extends TestCase
 {
@@ -16,6 +16,9 @@ public class SurveyReaderTest extends TestCase
     private MusicReader  musicReader;
 
 
+    /**
+     * creates an SurveyReader object for testing
+     */
     public void setUp() throws FileNotFoundException
     {
 
@@ -24,19 +27,14 @@ public class SurveyReaderTest extends TestCase
     }
 
 
+    /**
+     * tests to see if the SurveyReader can retrieve the songs from the File
+     */
     public void testGetStudents()
     {
         LList<Song> songs = musicReader.getSongs();
         LList<Student> students = reader.getStudents(songs);
-        for (int i = 1; i <= students.getLength(); i++)
-        {
-            Student student = students.getEntry(i);
-            System.out.println(student.toString());
-            for (int x = 1; x <= student.getSongsHeard().getLength(); x++)
-            {
-                System.out.println(student.getSongsHeard().getEntry(x));
-            }
-        }
+        assertEquals(6, students.getLength());
 
     }
 }
